236789ab03371a37fb9957df73e7a1c7b25d5007
Gour <157149036+anonymousthinker@users.noreply.github.com>
2025-04-13 09:08:04 +0530

    add files for basic books server

---
+++ server.js
@@ -0,0 +1,49 @@
+import { Hono } from "hono";
+
+const kv = await Deno.openKv();
+
+const staticRoutes = () => {
+  const app = new Hono();
+
+  app.get("/test", (c) => {
+    return c.text("Working Fine!");
+  });
+
+  app.get("/books", async (c) => {
+    const iter = kv.list({ prefix: ["books"] });
+    const books = [];
+    for await (const res of iter) {
+      books.push(res.value);
+    }
+
+    return c.json(books);
+  });
+
+  return app;
+};
+
+const authenticatedRoutes = () => {
+  const app = new Hono();
+
+  app.post("/add-book", async (c) => {
+    const body = await c.req.json();
+    const response = await kv.set(["books", body.title], body);
+    return c.json(response);
+  });
+
+  return app;
+};
+
+const createApp = () => {
+  const serveStaticRoutes = staticRoutes();
+  const serveAuthenticatedRoutes = authenticatedRoutes();
+
+  const app = new Hono();
+
+  app.route("/", serveStaticRoutes);
+  app.route("/", serveAuthenticatedRoutes);
+
+  return app;
+};
+
+Deno.serve(createApp().fetch);
